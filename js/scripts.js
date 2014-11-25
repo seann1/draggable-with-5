9981fd2ec18fbaf6e41eecf50968441b116443db
Sean <seanniesen@gmail.com>
2014-11-25 13:20:35 -0800

    start of new solution

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 $( init );
  
 function init() {
@@ -6,62 +7,34 @@ function init() {
 
   });
 
-  $('.destination1').droppable({
-  	drop: handleDropEvent1
+  $('.destination1, .destination2, .destination3, .destination4').droppable({
+  	drop: handleDropEvent,
 
   });
+ 
 
-  $('.destination2').droppable({
-  	drop: handleDropEvent2
 
-  });
-
-  $('.destination3').droppable({
-  	drop: handleDropEvent3
-
-  });
-
-  $('.destination4').droppable({
-  	drop: handleDropEvent4
-
-  });
-}
-
-var answerArray = [];
-
-function handleDropEvent1( event, ui ) {
-  var draggable = ui.draggable;
-  if (draggable.attr('id') === "square1") {
-  	answerArray.push("true");
-  }
-};
-
-function handleDropEvent2( event, ui ) {
-  var draggable = ui.draggable;
-  if (draggable.attr('id') === "square2") {
-  	answerArray.push("true");
-  }
-};
-
-function handleDropEvent3( event, ui ) {
-  var draggable = ui.draggable;
-  if (draggable.attr('id') === "square3") {
-  	answerArray.push("true");
-  }
-};
-
-function handleDropEvent4( event, ui ) {
-  var draggable = ui.draggable;
-  if (draggable.attr('id') === "square4") {
-  	answerArray.push("true");
-  }
 };
 
 
-function playIfTrue() {
-if (answerArray === ["true", "true", "true", "true"]) {
-  $('#video').attr({'autoplay':'true'});
-}
-}
 
-playIfTrue();
+function handleDropEvent( event, ui ) {
+  var draggable = ui.draggable;
+
+  var destNumber = $(this).attr('id');
+  var boxNumber = $(draggable).attr('id');
+
+
+
+  if (destNumber == boxNumber) {
+  	$(draggable).addClass("correct");
+  };
+
+  if (draggable.hasClass("correct")) {
+    alert("hello");
+  };
+
+  
+
+};
+
