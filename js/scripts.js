daf5b069016fea45949c8882f6b3647aa3a7142c
Sean <seanniesen@gmail.com>
2014-12-01 16:27:04 -0800

    done :camel:

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 
+$( document ).ready(function() {
+
+  function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+  var myArray = ['one','two','three','four','five'];
+  newArray = shuffle(myArray);
+
+  $(".square1").addClass(newArray[0]).attr('id', newArray[0]);
+  $(".square2").addClass(newArray[1]).attr('id', newArray[1]);
+  $(".square3").addClass(newArray[2]).attr('id', newArray[2]);
+  $(".square4").addClass(newArray[3]).attr('id', newArray[3]);
+  $(".square5").addClass(newArray[4]).attr('id', newArray[4]);
+
+});
+
+
+
 $( init );
  
 function init() {
-  $('.square1, .square2, .square3, .square4').draggable({
+  $('.square1, .square2, .square3, .square4, .square5').draggable({
   	cursor: 'move',
     stack: '.ui-draggable'
 
   });
 
-  $('.destination1, .destination2, .destination3, .destination4').droppable({
+  $('.destination1, .destination2, .destination3, .destination4, .destination5').droppable({
   	drop: handleDropEvent,
 
   });
@@ -35,7 +55,7 @@ function handleDropEvent( event, ui ) {
     draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
   };
 
-  if ($('.square1').hasClass("correct") && $('.square2').hasClass("correct") && $('.square3').hasClass("correct") && $('.square4').hasClass("correct")) {
+  if ($('.square1').hasClass("correct") && $('.square2').hasClass("correct") && $('.square3').hasClass("correct") && $('.square4').hasClass("correct") && $('.square5').hasClass("correct")) {
     document.getElementById('video').play();
   };
 
